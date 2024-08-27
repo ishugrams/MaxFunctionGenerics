0e8e31065f07fe734eef3a7ebb62bb8924a5d5d9
Isha Shinde <ishashinde2021@gmail.com>
2024-08-27 16:15:16 +0530

    Added: Finding maximum of Three Using Float

--- MaxFinderInteger.java
+++ MaxFinderInteger.java
@@ -1,36 +1,13 @@
-import java.util.Scanner;
-
-public class MaxFinderInteger {
-
-    public static Integer findMax(Integer num1, Integer num2, Integer num3) {
-        Integer max = num1;
-
-        if (num2 > max) {
-            max = num2;
-        }
-
-        if (num3 > max) {
-            max = num3;
-        }
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(maxint(10,25,12));
+    }
+    static int maxint(int a , int b , int c){
+        int max = 0;
+        max = Math.max(a , b);
+        max = Math.max(max , c);
 
         return max;
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter the first integer: ");
-        Integer num1 = scanner.nextInt();
-
-        System.out.print("Enter the second integer: ");
-        Integer num2 = scanner.nextInt();
-
-        System.out.print("Enter the third integer: ");
-        Integer num3 = scanner.nextInt();
-
-        Integer max = findMax(num1, num2, num3);
-        System.out.println("The maximum number is: " + max);
-        
-        scanner.close();
     }
 }
